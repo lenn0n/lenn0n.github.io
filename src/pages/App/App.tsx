c8b153289ab115061b3f97c3366e9d7ec14a5767
xennon <gameoveralisa@gmail.com>
2023-11-05 16:11:24 +0800

    Init files

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function App({}: Props) {
+  return (
+    <div style={{ color: 'magenta', fontSize: '60px'}}>TODO: CREATE ROUTER AND INSTALL TAILWIND</div>
+  )
+}
+
+export default App
