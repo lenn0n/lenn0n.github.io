4387603b8207bb716a2c9cde257b98948f9dfde0
xennon <gameoveralisa@gmail.com>
2023-12-27 00:27:44 +0800

    Updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,23 @@ type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div>Navbar</div>
+    <div className='bg-red-500 p-4'>
+      <div className="mx-auto container">
+        <div className="flex justify-between">
+          <div className="">
+            <div className="grid grid-cols-2">
+              <div className="hidden md:block">LARGE LOGO</div>
+              <div className="flex gap-4">
+                <div className="">About</div>
+                <div className="">Portfolio</div>
+                <div className="">Contact</div>
+              </div>
+            </div>
+          </div>
+          <div className="">SOCIAL MEDIA ACCOUNTS</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
